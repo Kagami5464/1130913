7aa136c037311f3703812248db6e5fce25904107
Kagami5464 <a0976919813@gmail.com>
2024-11-22 10:28:18 +0800

    1131122

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 //install: node js
 //install web server package: express >npm install express
-const { text } = require("body-parser");
 var express = require("express");
 var server = express();
 
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
-var DB = require("nedb-promises"); var Profolio = DB.create(__dirname+"/profolio.db");  Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+var DB = require("nedb-promises");
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
 
-/*server.get("/services", (req,res)=>{
+ 
+// ProfolioDB.insert([
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+// ])
+
+server.get("/services", (req, res)=>{
     //DB find
-    var Server=[
-        {icon: "fa-shopping-cart",heading:"E-Commerce",text:"Lorem ipsum dolor sit amet,consectetur"}
-        {icon: "fa-laptop",heading:"Responsive Design",text:"Lorem ipsum dolor sit amet,consectetur"}
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
     ];
-    res.send(Server);
+    res.send(Services);
 });
-*/
 
 server.get("/profolio", (req,res)=>{
-    //DB find 
-    res.send("Profolio");  })
+      //DB
+      ProfolioDB.find({}).then(results=>{
+        if(results != null){
+             res.send(results);
+        }else{
+            res.send("Error!");
+        }
+      })
+     
+
+})
+
 
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
